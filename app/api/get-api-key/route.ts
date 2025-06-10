a88109d2047143a74f968fc88e3dff5881369eb7
Md Jubayer <mdjubayerislamshuvo34@gmail.com>
2025-06-10 18:37:35 +0600

    fg

--- app/api/get-api-key/route.ts
+++ app/api/get-api-key/route.ts
@@ -1,6 +1,6 @@
 // app/api/get-api-key/route.ts
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "@/lib/authOptions";
 import { connectToDB } from "@/utils/db";
 import User from "@/models/User";
 
